71aad04bb2ccb1f3cfc578dfab3c4722485976eb
V3lbutreenn <tam35_ali@hotmail.com>
2025-04-10 00:05:42 +0300

    Giriş yapıldıktan sonra Admin sayfasına yönlendirmek için düzenleme yapıldı.

--- WebUI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ WebUI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -115,7 +115,7 @@ namespace WebUI.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return LocalRedirect("/Admin");
                 }
                 if (result.RequiresTwoFactor)
                 {
